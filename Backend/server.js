497964dd5c5bf36efc2739bc017e2cef37e39193
sahil8307 <sahilgarg3101@gmail.com>
2026-05-02 01:30:50 +0530

    add the dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -7,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost", // Uses DB_HOST from env, defaults to localhost if not found
-    user: process.env.DB_USER || "root",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
